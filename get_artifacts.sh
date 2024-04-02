df17f5f23eb2f0c737238e192c2840f460987996
Kolchintcev Dmitry <pollpad3@gmail.com>
2024-04-02 09:21:44 +0300

    Удалены башизмы

--- get_artifacts.sh
+++ get_artifacts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Скрипт принимает на вход имя джобы и возвращает последние ее артефакты от последнего успешного выполнения
 # Требует jq, unzip
@@ -8,25 +8,25 @@ ERROR=""
 
 ARTIFACT_FILE=${ARTIFACT_FILE:-"artifacts.zip"}
 
-function _usage() {
+_usage() {
    echo "${0##*/} <job name>"
 }
 
-function _get_artifacts() {
-    local _job_name=$1
+_get_artifacts() {
+    _job_name=$1
 
-    local _resp=$(curl -sS -q -f -LL --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "${CI_API_V4_URL}/projects/$CI_PROJECT_ID/jobs?scope=success")
-    if [[ -z $_resp  ]]; then
+    _resp=$(curl -sS -k -q -f -LL --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "${CI_API_V4_URL}/projects/$CI_PROJECT_ID/jobs?scope=success")
+    if [ -z "$_resp"  ]; then
         return 1
     fi
 
-    local _job_id=$(echo $_resp | jq --arg _job_name $_job_name '[.[] | select(.name == $_job_name)] |.[0].id')
-    if [[ -z "$_job_id" || $_job_id == "null" ]]; then
+    _job_id=$(echo "$_resp" | jq --arg _job_name "$_job_name" '[.[] | select(.name == $_job_name)] |.[0].id')
+    if [ -z "$_job_id" ] || [ "$_job_id" = "null" ]; then
         ERROR="Not found successfully jobs with name $_job_name"
         return 1
     fi
 
-    local _status=$(curl -sS -q -LL -w "%{http_code}"  -o $ARTIFACT_FILE --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "${CI_API_V4_URL}/projects/$CI_PROJECT_ID/jobs/$_job_id/artifacts")
+    _status=$(curl -sS -k -q -LL -w "%{http_code}"  -o "$ARTIFACT_FILE" --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "${CI_API_V4_URL}/projects/$CI_PROJECT_ID/jobs/$_job_id/artifacts")
     if [ $_status != 200 ]; then
         ERROR="Can't download atrifacts from job $_job_id. HTTP status: $_status"
         return 1
@@ -37,24 +37,23 @@ function _get_artifacts() {
 
 
 #############__main__#############
-if [[ ! $# -eq 1 ]]; then
+if [ ! $# -eq 1 ]; then
     _usage
     exit 1
 fi
 
 job_name=$1
 echo "Downloading artifacts from last successfull job $job_name..."
-_get_artifacts $job_name
+_get_artifacts "$job_name"
 if [ ! $? -eq 0 ]; then
-    echo $ERROR
+    echo "$ERROR"
     exit 1;
 fi
 
 echo "Extracting artifacts"
-unzip -o $ARTIFACT_FILE
-_get_artifacts $job_name
+unzip -o "$ARTIFACT_FILE"
 if [ ! $? -eq 0 ]; then
     exit 1;
 fi
 
-rm $ARTIFACT_FILE
+rm "$ARTIFACT_FILE"
